802571d7b430565bb322822b090a1674cfd11f42
serenebabu2022 <serenebabu22@gmail.com>
2024-05-03 17:56:50 +0100

    Google auth added

--- app/src/main/java/ie/setu/rentara_app/ui/auth/Login.kt
+++ app/src/main/java/ie/setu/rentara_app/ui/auth/Login.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import ie.setu.rentara_app.R
@@ -11,11 +13,16 @@ import ie.setu.rentara_app.ui.home.Home
 import ie.setu.rentara_app.databinding.LoginBinding
 import timber.log.Timber
 import androidx.lifecycle.Observer
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 
 class Login : AppCompatActivity() {
 
     private lateinit var loginRegisterViewModel : LoginRegisterViewModel
     private lateinit var loginBinding : LoginBinding
+    private lateinit var startForResult : ActivityResultLauncher<Intent>
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +37,8 @@ class Login : AppCompatActivity() {
             createAccount(loginBinding.fieldEmail.text.toString(),
                 loginBinding.fieldPassword.text.toString())
         }
+        loginBinding.googleSignInButton.setSize(SignInButton.SIZE_WIDE)
+        loginBinding.googleSignInButton.setColorScheme(0)
     }
 
     public override fun onStart() {
@@ -42,6 +51,7 @@ class Login : AppCompatActivity() {
 
         loginRegisterViewModel.firebaseAuthManager.errorStatus.observe(this, Observer
         { status -> checkStatus(status) })
+        loginBinding.googleSignInButton.setOnClickListener { googleSignIn() }
     }
 
     //Required to exit app from Login Screen - must investigate this further
@@ -91,5 +101,36 @@ class Login : AppCompatActivity() {
             loginBinding.fieldPassword.error = null
         }
         return valid
+    }
+    private fun googleSignIn() {
+        val signInIntent = loginRegisterViewModel.firebaseAuthManager
+            .googleSignInClient.value!!.signInIntent
+
+        startForResult.launch(signInIntent)
+    }
+
+    private fun setupGoogleSignInCallback() {
+        startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                when(result.resultCode){
+                    RESULT_OK -> {
+                        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                        try {
+                            // Google Sign In was successful, authenticate with Firebase
+                            val account = task.getResult(ApiException::class.java)
+                            loginRegisterViewModel.authWithGoogle(account!!)
+                        } catch (e: ApiException) {
+                            // Google Sign In failed
+                            Timber.i( "Google sign in failed $e")
+                            Snackbar.make(loginBinding.loginLayout, "Authentication Failed.",
+                                Snackbar.LENGTH_SHORT).show()
+                        }
+                        Timber.i("DonationX Google Result $result.data")
+                    }
+                    RESULT_CANCELED -> {
+
+                    } else -> { }
+                }
+            }
     }
 }
--- app/src/main/java/ie/setu/rentara_app/ui/auth/LoginRegisterViewModel.kt
+++ app/src/main/java/ie/setu/rentara_app/ui/auth/LoginRegisterViewModel.kt
@@ -3,6 +3,7 @@ package ie.setu.rentara_app.ui.auth
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.auth.FirebaseUser
 import ie.setu.rentara_app.firebase.FirebaseAuthManager
 
@@ -17,5 +18,8 @@ class LoginRegisterViewModel (app: Application) : AndroidViewModel(app) {
 
     fun register(email: String?, password: String?) {
         firebaseAuthManager.register(email, password)
+    }
+    fun authWithGoogle(acct: GoogleSignInAccount) {
+        firebaseAuthManager.firebaseAuthWithGoogle(acct)
     }
 }
